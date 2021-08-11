acae117fdc37fc5570d509421deae75a4b90ee2b
Thirumalai <thirumalai.s@presidio.com>
2021-08-11 12:46:30 +0530

    updated secondary user componenet

--- Fam-Budget/src/app/component/secondary-users/secondary-users/secondary-users.component.ts
+++ Fam-Budget/src/app/component/secondary-users/secondary-users/secondary-users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DashboardService } from 'src/app/service/dashboard/dashboard.service';
 import { FormModalComponent } from '../form-modal/form-modal.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
 }
@@ -9,17 +11,32 @@ export interface DialogData {
 @Component({
   selector: 'app-secondary-users',
   templateUrl: './secondary-users.component.html',
-  styleUrls: ['./secondary-users.component.scss']
+  styleUrls: ['./secondary-users.component.scss'],
+  
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class SecondaryUsersComponent implements OnInit {
   secondaryUsers: any;
   accountNumber : any;
+  active :any;
 
   constructor(public dialog: MatDialog, private dashboardService: DashboardService) {
     this.getSecondarUsers()
   }
 
   ngOnInit(): void {
+    this.active = true
   }
   movies:  Movie[] = [  
     {title:'Zootopia',director:'Byron Howard, Rich Moore',cast:'Idris Elba, Ginnifer Goodwin, Jason Bateman',releaseDate:'March 4, 2016'},  
@@ -30,6 +47,18 @@ export class SecondaryUsersComponent implements OnInit {
 openDialog() {
   this.dialog.open(FormModalComponent);
 }
+flip: string = 'inactive';
+
+activate() {
+  if(this.active == true){
+    this.active = false;
+  }
+  else{
+    this.active = true;
+  }
+
+}
+
   
 getSecondarUsers(){
   this.dashboardService.getSecondaryUsers(111).subscribe( (data) => {
